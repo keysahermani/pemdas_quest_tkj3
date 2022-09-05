afaca2454ebb58d5ec6eba6f589728265ab06263
keysahermani <112367428+keysahermani@users.noreply.github.com>
2022-09-04 22:40:54 -0700

    Add files via upload

---
+++ python6-tkj3.py
@@ -0,0 +1,17 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+==== RESTART: C:/Users/Administrator/Documents/keysa-python/python6-tkj3.py ====
+masukan nilai dolar : 9
+Traceback (most recent call last):
+  File "C:/Users/Administrator/Documents/keysa-python/python6-tkj3.py", line 4, in <module>
+    rupiah = dolar * 15000
+NameError: name 'dolar' is not defined
+
+==== RESTART: C:/Users/Administrator/Documents/keysa-python/python6-tkj3.py ====
+masukan nilai dolar : 8
+Dolar ke rupiah 120000
+masukan nilai euro: 8
+Euro ke Won 10856
+masukan nilai Yen: 8
+Yen Ke Rupiah 848
